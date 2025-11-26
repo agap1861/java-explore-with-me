2bcf192fb114cd6871f2dee7c20c958d8abc5ad0
Agapov.D <dani1aagapov@yandex.ru>
2025-11-26 15:06:45 +0100

    fix order

--- stat/stat-server/src/main/java/server/storage/StatJpaRepository.java
+++ stat/stat-server/src/main/java/server/storage/StatJpaRepository.java
@@ -15,26 +15,30 @@ public interface StatJpaRepository extends JpaRepository<HitEntity, Long> {
             "FROM HitEntity h " +
             "WHERE (timestamp BETWEEN ?1 AND ?2) " +
             "AND uri IN ?3 " +
-            "GROUP BY h.app, h.uri")
+            "GROUP BY h.app, h.uri " +
+            "ORDER BY COUNT(h.ip) DESC")
     List<Stat> getStatByUnuniqueIpAndUris(LocalDateTime start, LocalDateTime end, List<String> uris);
 
     @Query("SELECT new server.domain.Stat(h.app, h.uri, COUNT(DISTINCT h.ip))  " +
             "FROM HitEntity h " +
             "WHERE (timestamp BETWEEN ?1 AND ?2) " +
             "AND uri IN ?3 " +
-            "GROUP BY h.app, h.uri ")
+            "GROUP BY h.app, h.uri " +
+            "ORDER BY COUNT(h.ip) DESC ")
     List<Stat> getStatByUniqueIpANdUris(LocalDateTime start, LocalDateTime end, List<String> uris);
 
     @Query("SELECT new server.domain.Stat(h.app, h.uri, COUNT(h.ip)) " +
             "FROM HitEntity h " +
             "WHERE (timestamp BETWEEN ?1 AND ?2) " +
-            "GROUP BY h.app, h.uri")
+            "GROUP BY h.app, h.uri " +
+            "ORDER BY COUNT(h.ip) DESC")
     List<Stat> getStatByUnuniqueIpAndWithoutUris(LocalDateTime start, LocalDateTime end);
 
     @Query("SELECT new server.domain.Stat(h.app, h.uri, COUNT(DISTINCT h.ip)) " +
             "FROM HitEntity h " +
             "WHERE (timestamp BETWEEN ?1 AND ?2) " +
-            "GROUP BY h.app, h.uri")
+            "GROUP BY h.app, h.uri " +
+            "ORDER BY COUNT(h.ip) DESC")
     List<Stat> getStatByUniqueIpAndWithoutUris(LocalDateTime start, LocalDateTime end);
 
     @Query("SELECT new server.domain.Stat(h.app, h.uri, COUNT(h.ip)) FROM HitEntity h GROUP BY h.app, h.uri")
