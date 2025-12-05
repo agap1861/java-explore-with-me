f2abc20200b7649952eed67c1e9928d8616afdfb
Agapov.D <dani1aagapov@yandex.ru>
2025-12-05 23:07:01 +0100

    test

--- main-service/src/main/java/ewm/comment/service/CommentServiceImpl.java
+++ main-service/src/main/java/ewm/comment/service/CommentServiceImpl.java
@@ -25,77 +25,83 @@ public class CommentServiceImpl implements CommentService {
     @Override
     @Transactional
     public Comment postComment(Comment comment, Long authorId, Long eventId) throws NotFoundException, ConditionsNotMetException {
-        Event event = eventService.getEventById(eventId);
+ /*       Event event = eventService.getEventById(eventId);
         if (!event.getState().equals(EventState.PUBLISHED)) {
             throw new ConditionsNotMetException("event must be published");
         }
         User author = userService.getUserById(authorId);
         comment.setAuthor(author);
         comment.setEvent(event);
-        return storage.save(comment);
+        return storage.save(comment);*/
+        return null;
     }
 
     @Override
     @Transactional
     public Comment patchComment(Long authorId, Long eventId, Long commentId, Comment patchComment) throws NotFoundException, ConditionsNotMetException {
-        Comment comment = getCommentById(commentId);
+/*        Comment comment = getCommentById(commentId);
         validateAuthorAndEvent(comment, authorId, eventId);
         comment.setText(patchComment.getText());
-        return storage.save(comment);
+        return storage.save(comment);*/
+        return null;
     }
 
     @Override
     public Comment getCommentById(Long commentId) throws NotFoundException {
-        return storage.getById(commentId).orElseThrow(
+/*        return storage.getById(commentId).orElseThrow(
                 () -> new NotFoundException("comment with id " + commentId + " does not exist")
-        );
+        );*/
+        return null;
     }
 
     @Override
     @Transactional
     public void userDeleteComment(Long authorId, Long eventId, Long commentId) throws NotFoundException, ConditionsNotMetException {
-        Comment comment = getCommentById(commentId);
+/*        Comment comment = getCommentById(commentId);
         validateAuthorAndEvent(comment, authorId, eventId);
-        storage.delete(commentId);
+        storage.delete(commentId);*/
 
     }
 
     @Override
     @Transactional
     public void adminDeleteComment(Long commentId) throws NotFoundException {
-        if (!storage.existById(commentId)) {
+/*        if (!storage.existById(commentId)) {
             throw new NotFoundException("comment with id " + commentId + " does not exist");
         }
-        storage.delete(commentId);
+        storage.delete(commentId);*/
     }
 
     @Override
     public List<Comment> getAllCommentForAdminByEventId(Long eventId) throws NotFoundException {
-        if (!eventService.existById(eventId)) {
+/*        if (!eventService.existById(eventId)) {
             throw new NotFoundException("event with id " + eventId + " does not exist");
         }
-        return storage.getAllCommentsByEventId(eventId);
+        return storage.getAllCommentsByEventId(eventId);*/
+        return null;
     }
 
     @Override
     public List<Comment> getAllCommentsForUserByEventId(Long userId, Long eventId) throws NotFoundException {
-        if (!userService.existById(userId)) {
+/*        if (!userService.existById(userId)) {
             throw new NotFoundException("only authorized user  can watch comments");
         }
         if (!eventService.existById(eventId)) {
             throw new NotFoundException("event with id " + eventId + " does not exist");
         }
-        return storage.getAllCommentsByEventId(eventId);
+        return storage.getAllCommentsByEventId(eventId);*/
+        return null;
     }
 
     private void validateAuthorAndEvent(Comment comment, Long authorId, Long eventId) throws ConditionsNotMetException {
-        if (!comment.getAuthor().getId().equals(authorId)) {
+/*        if (!comment.getAuthor().getId().equals(authorId)) {
             throw new ConditionsNotMetException("this comment does not belong current user");
         }
         if (!comment.getEvent().getId().equals(eventId)) {
             throw new ConditionsNotMetException("this comment does not belong current event");
 
-        }
+        }*/
+
     }
 
 
